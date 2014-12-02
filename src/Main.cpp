7a9e0f6f2e1acaf4db68f186a637c514dae495b6
Thomask <thomasking0529@gmail.com>
2014-12-02 13:59:58 +0800

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,26 +10,31 @@
 #include "Core.hpp"
 
 int main(int argc, char *argv[]) {
-	Lexer lex;
-	std::list<std::string> a = lex.split(
-			"select * from a where(id == a || id == 0)");
-	for (auto& i : a) {
-		std::cout << i << std::endl;
-	}
-	std::list<Token> s = lex.GetTokens(
-			"select * from a where(id == a || id == 0 && id == s)");
-	for (auto& i : s) {
-		std::cout << i.type << " " << i.value << std::endl;
-	}
-//	SimpleDB* db = new SimpleDB();
+	SimpleDB* db = new SimpleDB();
 	if (argc == 2) {
 		//open file
+		std::string es;
 		std::ifstream fin(argv[1]);
-		//execute tests
-//		db->Execute("select * from a");
-		std::cout << "Line :" << std::endl;
+
+		int line_count = 1;
+
+		while (std::getline(fin, es)) {
+			std::cout << "Line " << line_count << ": " << es << std::endl;
+			db->Execute(es);
+			std::cout << "Line :" << std::endl;
+		}
 	} else {
-		//open terminal
+		Lexer lex;
+		std::list<std::string> a = lex.split(
+				"select * from a where(id == a || id == 0)");
+		for (auto& i : a) {
+			std::cout << i << std::endl;
+		}
+		std::list<Token> s = lex.GetTokens(
+				"select * from a where(id == a || id == 0 && id == s)");
+		for (auto& i : s) {
+			std::cout << i.type << " " << i.value << std::endl;
+		}
 	}
 	return 0;
 }
