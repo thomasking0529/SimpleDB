3eb00dc4d9da6656a727dabcf12b0ed337382640
QZL <gek_u@foxmail.com>
2014-12-27 17:35:47 +0800

    V2

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,9 +5,8 @@
  *      Author: thomas
  */
 
-#define _CRT_SECURE_NO_WARNINGS
-#define RULE "Rule_1.txt"
-#define TERMINAL "Terminal_list.txt"
+#define RULE "Rule_2.txt"
+#define TERMINAL "Terminal_list_2.txt"
 #include <string>
 #include <cstring>
 #include <stack>
@@ -27,16 +26,16 @@ void Parser::initTable() {
 		printf("sssss");
 		return;
 	}
-	char rule[100], *next;
+	char rule[200], *next;
 	std::string top, front, content;
-	while (fgets(rule, 100, file)) {
+	while (fgets(rule, 200, file)) {
 		std::list<std::string> lst;
-		top = strtok(rule, " ");
-		front = strtok(NULL, " ");
-		next = strtok(NULL, " ");
+		top = strtok(rule, " \n");
+		front = strtok(NULL, " \n");
+		next = strtok (NULL, " \n");
 		while (next != NULL) {
 			lst.push_back(next);
-			next = strtok(NULL, " ");
+			next = strtok (NULL, " \n");
 		}
 		table[std::make_pair(top, front)] = lst;
 	}
@@ -48,7 +47,7 @@ void Parser::initTerminal() {
 		printf("sssss");
 		return;
 	}
-	char rule[100];
+	char rule[200];
 	std::string id = "id", num = "num";
 	while (!feof(file)) {
 		fscanf(file, "%s", rule);
@@ -58,24 +57,21 @@ void Parser::initTerminal() {
 }
 
 void Parser::initAction() {
-	std::for_each(terminal.begin(), terminal.end(),
-			[&](const std::string& ter) {
-				action[ter] = [](Statement &s, Token t, std::string father) {
-					//printf("%s", t.value.c_str());
-				};
-			});
-	action["select"] =
-			[](Statement &s, Token t, std::string father) {s.act = QUERY;};
-	action["create"] =
-			[](Statement &s, Token t, std::string father) {s.act = CREATE;};
-	action["insert"] =
-			[](Statement &s, Token t, std::string father) {s.act = INSERT;};
-	action["delete"] =
-			[](Statement &s, Token t, std::string father) {s.act = DELETE;};
-	action["id"] = [](Statement &s, Token t, std::string father) {
+	std::for_each(terminal.begin(), terminal.end(), 
+		[&](const std::string& ter){
+			action[ter] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+				//printf("%s", t.value.c_str());
+			};
+	});
+	action["select"] = [](Statement &s, Token t, std::string father, Polish* &calc) {s.act = QUERY;};
+	action["create"] = [](Statement &s, Token t, std::string father, Polish* &calc) {s.act = CREATE;};
+	action["insert"] = [](Statement &s, Token t, std::string father, Polish* &calc) {s.act = INSERT;};
+	action["delete"] = [](Statement &s, Token t, std::string father, Polish* &calc) {s.act = DELETE;};
+	action["where"] = [](Statement &s, Token t, std::string father, Polish* &calc) {calc = new Polish();};
+	action["id"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
 		if (father == "create_stmt" || father == "insert_stmt"
-				|| father == "delete_stmt" || father == "query_stmt") {
-			s.table = t.value;
+			|| father == "delete_stmt" || father == "query_stmt"){
+				s.table = t.value;
 		} else if (father == "decl") {
 			Property p;
 			p.id = t.value;
@@ -90,186 +86,176 @@ void Parser::initAction() {
 				p.default_value = 0;
 				s.prop_list.push_back(p);
 			}
-		} else if (father == "operand") {
-			s.treeInsert(t);
+		} else if (calc != NULL && father == "unary") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-	action["num"] = [](Statement &s, Token t, std::string father) {
-		if (father == "default_spec") {
-			Property p = s.prop_list.back();
-			p.default_value = atoi(t.value.c_str());
-			s.prop_list.pop_back();
-			s.prop_list.push_back(p);
-		} else if (father == "value_list") {
-			s.value_list.push_back(atoi(t.value.c_str()));
-		} else if (father == "operand") {
-			s.treeInsert(t);
+	action["num"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (father == "simple_unary") {
+			if (calc == NULL) calc = new Polish();
+			calc->Insert(t.value);
+		} else if (calc != NULL && father == "unary") {
+			calc->Insert(t.value);
+		} else {
+			//throw SDBException("something wrong");
+		}
+	};
+	action["<="] = action[">="] = action["<"] = action[">"] = action["=="] = action["<>"]
+	= [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "rop") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-	action["<>"] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
+	action["&&"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "conjunct*") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-	action["=="] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
+	action["||"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "disjunct*") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-	action[">"] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
+	action["!"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "bool") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-	action["<"] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action[">="] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action["<="] = [](Statement &s, Token t, std::string father) {
-		if (father == "rop") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action["&&"] = [](Statement &s, Token t, std::string father) {
-		if (father == "conjunct_list*") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action["||"] = [](Statement &s, Token t, std::string father) {
-		if (father == "conjunct_list*") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action["!"] = [](Statement &s, Token t, std::string father) {
-		if (father == "conjunct_list*") {
-			s.treeInsert(t);
-		} else {
-			throw SDBException("something wrong");
-		}
-	};
-	action["*"] = [](Statement &s, Token t, std::string father) {
+	action["*"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
 		if (father == "select_list") {
 			Property p;
 			p.id = t.value;
 			p.default_value = 0;
 			s.prop_list.push_back(p);
+		} else if (calc != NULL && father == "simple_term*"){
+			calc->Insert(t.value);
+		} else if (calc != NULL && father == "term*") {
+			calc->Insert(t.value);
 		} else {
 			throw SDBException("something wrong");
 		}
 	};
-}
-
-void Statement::treeInsert(Token node) {
-	if (cond == NULL) {
-		cond = new Condition();
-	}
-	Condition *toInsert = insertNodeSearch(cond);
-	switch (node.type) {
-	case ID: {
-		if (toInsert->lc == NULL) {
-			Condition *newNode = new Condition();
-			newNode->opd = node.value;
-			toInsert->lc = newNode;
+	action["/"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "simple_term*"){
+			calc->Insert(t.value);
+		} else if (calc != NULL && father == "term*") {
+			calc->Insert(t.value);
 		} else {
-			Condition *newNode = new Condition();
-			newNode->opd = node.value;
-			toInsert->rc = newNode;
+			throw SDBException("something wrong");
 		}
-		break;
-	}
-	case NUM: {
-		if (toInsert->lc == NULL) {
-			Condition *newNode = new Condition();
-			newNode->opd = node.value;
-			toInsert->lc = newNode;
+	};
+	action["+"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "simple_expr*"){
+			calc->Insert(t.value);
+		} else if (father == "simple_unary"){
+			if (calc == NULL) {
+				calc = new Polish();
+			}
+		} else if (calc != NULL && father == "expr*"){
+			calc->Insert(t.value);
 		} else {
-			Condition *newNode = new Condition();
-			newNode->opd = node.value;
-			toInsert->rc = newNode;
+			throw SDBException("something wrong");
 		}
-		break;
-	}
-	case OP: {
-		/*
-		 LT, // <
-		 GT, // >
-		 NE, // <>
-		 EQ, // ==
-		 GTE, // >=
-		 LTE, // <=
-		 AND, // &&
-		 OR, // ||
-		 NOT, // !
-		 */
-		if (node.value == "<") {
-			toInsert->op = LT;
-		} else if (node.value == ">") {
-			toInsert->op = GT;
-		} else if (node.value == "<>") {
-			toInsert->op = NE;
-		} else if (node.value == "==") {
-			toInsert->op = EQ;
-		} else if (node.value == ">=") {
-			toInsert->op = GTE;
-		} else if (node.value == "<=") {
-			toInsert->op = LTE;
-		} else if (node.value == "&&") {
-			rotate(cond);
-			cond->op = AND;
-		} else if (node.value == "||") {
-			rotate(cond);
-			cond->op = OR;
-		} else if (node.value == "!") {
-			rotate(cond);
-			cond->op = NOT;
+	};
+	action["-"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "simple_expr*"){
+			calc->Insert(t.value);
+		} else if (father == "simple_unary"){
+			if (calc == NULL) {
+				calc = new Polish();
+			}
+			calc->neglect();
+		} else if (calc != NULL && father == "expr*"){
+			calc->Insert(t.value);
+		} else if (calc != NULL && father == "unary"){
+			calc->neglect();
+		} else {
+			throw SDBException("something wrong");
 		}
-		break;
-	}
-	default:
-		throw SDBException("abcdefg");
-	}
+	};	
+	action["("] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (father == "simple_unary"){
+			if (calc == NULL) {
+				calc = new Polish();
+			}
+			calc->Insert(t.value);
+		} else if (calc != NULL && father == "bool"){
+			calc->Insert(t.value);
+		} else if (father == "create_stmt" || father == "insert_stmt"){
+		} else {
+			throw SDBException("something wrong");
+		}
+	};
+	action[")"] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && father == "simple_unary"){
+			calc->Insert(t.value);
+		} else if (father == "create_stmt" || father == "insert_stmt"){
+			if (calc != NULL) {
+				int result = calc->Calculate();
+				switch (s.act) {
+				case CREATE:
+					{
+						Property p = s.prop_list.back();
+						p.default_value = result;
+						s.prop_list.pop_back();
+						s.prop_list.push_back(p);
+						break;
+					}
+				case INSERT:
+					{
+						s.value_list.push_back(result);
+						break;
+					}
+				}
+				delete calc;
+				calc = NULL;
+			} else {
+				// ok
+			}
+		} else if (father == "bool") {
+			calc->Insert(t.value);
+		} else {
+			throw SDBException("something wrong");
+		}
+	};
+	action[","] = [](Statement &s, Token t, std::string father, Polish* &calc) {
+		if (calc != NULL && (father == "value_list*" || father == "decl_list*")){
+			int result = calc->Calculate();
+			switch (s.act) {
+				case CREATE:
+					{
+						Property p = s.prop_list.back();
+						p.default_value = result;
+						s.prop_list.pop_back();
+						s.prop_list.push_back(p);
+						break;
+					}
+				case INSERT:
+					{
+						s.value_list.push_back(result);
+						break;
+					}
+			}
+			delete calc;
+			calc = NULL;
+		} else if (father == "column_list*" || father == "insert_stmt" || father == "decl_list*"){
+		} else {
+			throw SDBException("something wrong");
+		}
+	};
+
 }
 
-Condition* Statement::insertNodeSearch(Condition* &cond) {
-	if (cond->lc == NULL || cond->rc == NULL) {
-		return cond;
-	} else {
-		return insertNodeSearch(cond->lc);
-	}
-}
-
-void Statement::rotate(Condition* &cond) {
-	Condition* tmp = cond;
-	cond = new Condition();
-	cond->rc = tmp;
-	cond->lc = new Condition();
-}
 
 std::string Parser::getTokenSymbol(Token t) {
 	if (t.type == NUM) {
@@ -289,29 +275,33 @@ Statement Parser::Parse(const std::string& s) {
 	Statement st;
 	procedure.push("$");
 	procedure.push("ssql_stmt");
+	int ip = 0;
 	std::string top = procedure.top();
 	std::stack<std::string> father;
 	father.push("$");
 	father.push("ssql_stmt");
 	std::string fa;
 	fa = father.top();
+	Polish* calc = NULL;
 	while (top != "$") {
 		father.pop();
 		if (top == "#") {
 			procedure.pop();
 		} else if (terminal.count(top) > 0) {
 			if (ts.front().type != ID && ts.front().type != NUM
-					&& ts.front().value != top) {
+				&& ts.front().value != top) {
 				throw SDBException("unexpected token");
 				break;
 			}
-			action[top](st, ts.front(), fa);
+			printf("%s    %s   \n", top.c_str(), fa.c_str());
+			action[top](st, ts.front(), fa, calc);
 			ts.pop_front();
 			procedure.pop();
+			//printf("next:  %s    %s   \n", ts.front().value.c_str(), procedure.top().c_str());
 		} else {
 			procedure.pop();
-			std::list<std::string> af = table[std::make_pair(top,
-					getTokenSymbol(ts.front()))];
+			std::list<std::string> af
+				= table[std::make_pair(top, getTokenSymbol(ts.front()))];
 			std::list<std::string>::reverse_iterator i = af.rbegin();
 			for (; i != af.rend(); ++i) {
 				father.push(top);
@@ -321,10 +311,13 @@ Statement Parser::Parse(const std::string& s) {
 		fa = father.top();
 		top = procedure.top();
 	}
-	if (top != "$")
-		throw SDBException("unknown error");
+	if (top != "$") throw SDBException("unknown error");
+	if (calc != NULL && (st.act == DELETE || st.act == QUERY)) {
+		st.cond = calc->buildTree();
+	}
 	return st;
 }
+
 
 Condition* Parser::parseWhere(std::list<Token> ts) {
 	//example:
