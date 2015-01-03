31ef4ef51b659fad79daa877dd37ca4dcb15ef74
QZL <gek_u@foxmail.com>
2015-01-03 09:17:08 +0800

    加注释

--- src/Parser.hpp
+++ src/Parser.hpp
@@ -145,365 +145,115 @@ struct Statement {
 
 };
 
-//reverse polish calculater
+/*
+ * reverse polish calculater, the parser function use it to translate math
+ * expression into number and translate bool expression to a condition
+ * tree.
+ */
 class Polish {
 private:
+	/*
+	 * flag, isNeglect, and count used for deal with the higher priority
+	 * of - in express "-num" or "-id"
+	 */
 	bool flag;
 	int isNeglect;
 	int count;
+
+	/*
+	 * data structure used for translation to reverse polish expression
+	 */
 	std::list<std::string> ops;
 	std::queue<std::string> rp;
 	std::map<std::string, int> level;
 	std::set<std::string> _ops;
 public:
-	Polish() {
-		level.insert(std::pair<std::string, int>("+", 3));
-		level.insert(std::pair<std::string, int>("-", 3));
-		level.insert(std::pair<std::string, int>("*", 4));
-		level.insert(std::pair<std::string, int>("/", 4));
-		level.insert(std::pair<std::string, int>("==", 1));
-		level.insert(std::pair<std::string, int>("<>", 1));
-		level.insert(std::pair<std::string, int>(">", 2));
-		level.insert(std::pair<std::string, int>("<", 2));
-		level.insert(std::pair<std::string, int>(">=", 2));
-		level.insert(std::pair<std::string, int>("<=", 2));
-		level.insert(std::pair<std::string, int>("!", 0));
-		level.insert(std::pair<std::string, int>("&&", -1));
-		level.insert(std::pair<std::string, int>("||", -2));
-		level.insert(std::pair<std::string, int>("(", -3));
-		level.insert(std::pair<std::string, int>(")", -3));
-		_ops.insert("+");
-		_ops.insert("-");
-		_ops.insert("*");
-		_ops.insert("/");
-		_ops.insert("<");
-		_ops.insert(">");
-		_ops.insert("<=");
-		_ops.insert(">=");
-		_ops.insert("==");
-		_ops.insert("<>");
-		_ops.insert("&&");
-		_ops.insert("||");
-		_ops.insert("!");
-		isNeglect = 0;
-		count = -1;
-		flag = false;
-	}
+	/*
+	 * constructor, init basic priority
+	 */
+	Polish();
+
+	/* 
+	 * - in expression  "-num" or "-id" found
+	 */
+	void neglect();
+
+	/*
+	 * call when parser found a token in a expression, tranlate
+	 * the expression by tokens
+	 */
+	void Insert(const std::string& item);
+
 	
-	//check neglect number
-	void neglect() {
-		flag = !flag;
-	}
+	/*
+	 * if the math expression is translate, calculate the value of
+	 * the expression.
+	 */
+	int Calculate();
 
-	//get input tokens
-	void Insert(const std::string& item) {
-		if (_ops.find(item) != _ops.end()) {
-			if (item == "!") Insert("!!");
-			while (!ops.empty() &&
-				level.find(item)->second <= level.find(ops.back())->second) {
-				rp.push(ops.back());
-				ops.pop_back();
-			}
-			ops.push_back(item);
-		} else if (item == "(") {
-			if (flag) {
-				flag = false;
-				ops.push_back("(");
-				rp.push("0");
-				Insert("-");
-				isNeglect++;
-				count++;
-			} else if (count != -1) {
-				isNeglect++;
-			}
-			ops.push_back("(");
-		} else if (item == ")") {
-			if (count != -1) {
-				isNeglect--;
-				if (isNeglect == count) {
-					while (ops.back() != "(") {
-						rp.push(ops.back());
-						ops.pop_back();
-					}
-					ops.pop_back();
-					count--;
-				}
-			}
-			while (ops.back() != "(") {
-				rp.push(ops.back());
-				ops.pop_back();
-			}
-			ops.pop_back();
-		} else {
-			if (flag) {
-				int tm;
-				if (isNum(tm, item)) {
-					int result = -tm;
-					char a[100];
-					sprintf(a, "%d", result);
-					rp.push(std::string(a));
-				} else {
-					ops.push_back("(");
-					rp.push("0");
-					Insert("-");
-					rp.push(item);
-					while (ops.back() != "(") {
-						rp.push(ops.back());
-						ops.pop_back();
-					}
-					ops.pop_back();
-				}
-				flag = false;
-			} else {
-				rp.push(item);
-			}
-		}
-	}
+	/*
+	 * if the bool expression is translate, build a condition tree of
+	 * the expression.
+	 */
+	Condition* buildTree();
 
-	//calculate inputs
-	int Calculate() {
-		std::stack<int> nums;
-		while (!ops.empty()) {
-			rp.push(ops.back());
-			ops.pop_back();
-		}
-		while (!rp.empty()) {
-			std::string to_ret = rp.front();
-			rp.pop();
-			if (_ops.find(to_ret) != _ops.end()) {
-				if (nums.size() < 2) {
-					std::cerr << "Wrong expression.\n";
-					return 0;
-				} else {
-					int b = nums.top();
-					nums.pop();
-					int a = nums.top();
-					nums.pop();
-					if (to_ret == "+") {
-						nums.push(a + b);
-					} else if (to_ret == "*") {
-						nums.push(a * b);
-					} else if (to_ret == "-") {
-						nums.push(a - b);
-					} else if (to_ret == "/") {
-						if (b == 0) {
-							throw SDBException("can't devided by zero");
-						}
-						nums.push(a / b);
-					}
-				}
-			} else {
-				nums.push(atoi(to_ret.c_str()));
-			}
-		}
-		if (nums.size() != 1) {
-			std::cerr << "Wrong expression.\n";
-			return 0;
-		} else {
-			int t = nums.top();
-			nums.pop();
-			return t;
-		}
-	}
-
-	//build condition tree
-	Condition* buildTree() {
-		Condition *t = new Condition();
-		std::stack<Condition*> nums;
-		while (!ops.empty()) {
-			rp.push(ops.back());
-			ops.pop_back();
-		}
-		while (!rp.empty()) {
-			std::string to_ret = rp.front();
-			rp.pop();
-			if (_ops.find(to_ret) != _ops.end()) {
-				if (nums.size() < 2) {
-					std::cerr << "Wrong expression.\n";
-					return 0;
-				} else {
-					Condition* b = nums.top();
-					nums.pop();
-					Condition* a = nums.top();
-					nums.pop();
-					if (to_ret == "+") {
-						int numa, numb;
-						if (isNum(numa, a->opd) && isNum(numb, b->opd)) {
-							int result = numa + numb;
-							char a[100];
-							sprintf(a, "%d", result);
-							nums.push(new Condition(a));
-						} else {
-							Condition* re = new Condition();
-							re->lc = a;
-							re->rc = b;
-							re->opd = "no num";
-							re->op = PLUS;
-							nums.push(re);
-						}
-					} else if (to_ret == "*") {
-						int numa, numb;
-						if (isNum(numa, a->opd) && isNum(numb, b->opd)) {
-							int result = numa * numb;
-							char a[100];
-							sprintf(a, "%d", result);
-							nums.push(new Condition(a));
-						} else {
-							Condition* re = new Condition();
-							re->lc = a;
-							re->rc = b;
-							re->opd = "no num";
-							re->op = MULTIPLY;
-							nums.push(re);
-						}
-					} else if (to_ret == "-") {
-						int numa, numb;
-						if (isNum(numa, a->opd) && isNum(numb, b->opd)) {
-							int result = numa - numb;
-							char a[100];
-							sprintf(a, "%d", result);
-							nums.push(new Condition(a));
-						} else {
-							Condition* re = new Condition();
-							re->lc = a;
-							re->rc = b;
-							re->opd = "no num";
-							re->op = MINUS;
-							nums.push(re);
-						}
-					} else if (to_ret == "/") {
-						int numa, numb;
-						if (isNum(numa, a->opd) && isNum(numb, b->opd)) {
-							if (numb == 0) {
-								throw SDBException("can't devided by zero");
-							}
-							int result = numa / numb;
-							char a[100];
-							sprintf(a, "%d", result);
-							nums.push(new Condition(a));
-						} else {
-							Condition* re = new Condition();
-							re->lc = a;
-							re->rc = b;
-							re->opd = "no num";
-							re->op = DIVIDE;
-							nums.push(re);
-						}
-					} else if (to_ret == "<"){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = LT;
-						nums.push(re);
-					} else if (to_ret == ">"){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = GT;
-						nums.push(re);
-					} else if (to_ret == "<="){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = LTE;
-						nums.push(re);
-					} else if (to_ret == ">="){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = GTE;
-						nums.push(re);
-					} else if (to_ret == "<>"){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = NE;
-						nums.push(re);
-					} else if (to_ret == "=="){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = EQ;
-						nums.push(re);
-					} else if (to_ret == "&&"){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = AND;
-						nums.push(re);
-					} else if (to_ret == "||"){
-						Condition* re = new Condition();
-						re->lc = a;
-						re->rc = b;
-						re->opd = "no num";
-						re->op = OR;
-						nums.push(re);
-					} else if (to_ret == "!") {
-						if (a->opd == "!!" && b->opd == "!!") {
-							nums.push(a);
-						} else if (a->opd == "!!") {
-							Condition* re = new Condition();
-							re->lc = b;
-							re->opd = "no num";
-							re->op = NOT;
-							nums.push(re);
-						} else {
-							std::cerr << "Wrong expression.\n";
-							return 0;
-						}
-					}
-				} 
-			} else {
-				Condition* tmp = new Condition();
-				tmp->opd = to_ret;
-				nums.push(tmp);
-			}
-		}
-		if (nums.size() != 1) {
-			std::cerr << "Wrong expression.\n";
-			return 0;
-		} else {
-			Condition* t = nums.top();
-			nums.pop();
-			return t;
-		}
-	}
-
-	bool isNum(int &num, std::string str) {
-		int i = 0;
-		while (str[i] == '-') i++;
-		for (; i < str.size(); i++) {
-			if (str[i] > '9' || str[i] < '0') {
-				return false;
-			}
-		}
-		num = std::atoi(str.c_str());
-		return true;
-	}
+	/*
+	 * common fuction for testing  whether a string is an number
+	 * and if it is, the number store in num.
+	 */
+	bool isNum(int &num, std::string str);
 };
 
 class Parser {
 private:
+	/*
+	 * lexer is where the tokens from
+	 */
 	Lexer* lexer;
+
+	/*
+	 * the action function definision, valid token t need to translate into the 
+	 * Statement s,  given the Grammar Productions' left part where this token
+	 * is produced to help the translation, and if token is in an expression, the
+	 * Polish calc will be useful.
+	 */
+	typedef void (*act)(Statement &s, Token t, std::string father, Polish* &calc);
+
+	/*
+	 * the following three function is to init those table, set and map
+	 * by reading the rule file we make by LL1. table gives the predictive
+	 * parsing table, terminal defines which token is terminal, and the 
+	 * action is the action we should take to make translattion when we
+	 * recognize an valid token
+	 */
 	void initTable();
 	void initTerminal();
 	void initAction();
-	typedef void (*act)(Statement &s, Token t, std::string father, Polish* &calc);
 	std::map<std::pair<std::string, std::string>, std::list<std::string> > table;
 	std::set<std::string> terminal;
 	std::map<std::string, act> action;
+
+
+	/*
+	 * procedure is the stack use for predictive parsing
+	 */
 	std::stack<std::string> procedure;
+
+	/*
+	 * common function to get the token sysbol in the  grammar production
+	 * to see whether the token match a sysbol in production.
+	 */
 	std::string getTokenSymbol(Token t);
+
+	/*
+	 * turn a num into string
+	 */
 	static std::string intToString(int num);
 public:
 	Parser();
+	/*
+	 * main parse function
+	 */
 	Statement Parse(const std::string& s);
 };
 
